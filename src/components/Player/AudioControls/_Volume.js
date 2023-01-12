94dd89a7903ec91c414f407004e73475bcb04117
michalsut <sut.michal@gmail.com>
2023-01-12 16:05:14 +0100

    WIP volume control

--- src/components/Player/AudioControls/_Volume.js
+++ src/components/Player/AudioControls/_Volume.js
@@ -2,5 +2,29 @@ import {CiVolumeHigh} from "react-icons/ci";
 import React from "react";
 
 export default function Volume({level, onChange}){
-    return <CiVolumeHigh size="2em" onClick={(e) => onChange(0)}/>
+
+    return (
+        <>
+            {/*<div><input type="range" width="5px" height="100px" /></div>*/}
+            <div style={{width: "30px"}} id="volume-container">
+                <div id="volume-button" style={{paddingTop: "10px", marginTop: "-10px", cursor: "pointer"}}>
+                    <CiVolumeHigh size="2em" onClick={(e) => onChange(0)}/>
+                </div>
+                <div id="volume-slider"
+                    style={{
+                    // display: "none",
+                    paddingTop: "5px",
+                    paddingBottom: "5px",
+                    position: "absolute",
+                    bottom: "50px",
+                    backgroundColor: "white",
+                    width: "30px",
+                    border: "1px solid lightgray",
+                    borderRadius: "15px"
+                }}>
+                    <input className="uk-range uk-inline " type="range" min="-100" max="100" step="10" orient="vertical"/>
+                </div>
+            </div>
+        </>
+    )
 }
